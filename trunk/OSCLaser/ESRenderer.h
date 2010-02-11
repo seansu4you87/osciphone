d8612b2757c291dd885ec8ef59a9808533384da0
codeb87 <codeb87@b5d3d9cc-0eb8-11df-a223-d798cd56f61e>
2010-02-11 09:28:32 +0000

    beginning GL implementation

---
+++ trunk/OSCLaser/ESRenderer.h
@@ -0,0 +1,19 @@
+//
+//  ESRenderer.h
+//  gl_test
+//
+//  Created by Ben Cunningham on 2/11/10.
+//  Copyright Stanford University 2010. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+#import <OpenGLES/EAGL.h>
+#import <OpenGLES/EAGLDrawable.h>
+
+@protocol ESRenderer <NSObject>
+
+- (void) render;
+- (BOOL) resizeFromLayer:(CAEAGLLayer *)layer;
+
+@end
