b73300403fb477e2892b4e74c0bbb9d6b70abb28
codeb87 <codeb87@b5d3d9cc-0eb8-11df-a223-d798cd56f61e>
2010-02-16 03:24:14 +0000

    separation of touch controllers

---
+++ OSCLaser/Classes/SequencerController.mm
@@ -0,0 +1,39 @@
+//
+//  SequencerController.m
+//  OSCLaser
+//
+//  Created by Ben Cunningham on 2/15/10.
+//  Copyright 2010 Stanford University. All rights reserved.
+//
+
+#import "SequencerController.h"
+
+
+@implementation SequencerController
+
+- (id) initWithParentView:(UIView*)theParentView
+{
+	if(self = [super init])
+	{
+		parentView = theParentView;
+	}
+	
+	return self;
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	
+}
+
+@end
